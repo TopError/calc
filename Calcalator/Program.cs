30201d469575cbdebc5e6c2800a8df7e251d472d
TopError <German.Luhsheider@yandex.ru>
2023-01-19 14:38:57 +0300

    First comm

---
+++ Calcalator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calcalator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
